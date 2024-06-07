0e52dc6c685f15349d2e8f6d9eaa1f4617473716
MohamedSalman2371999 <95053768+MohamedSalman2371999@users.noreply.github.com>
2024-06-07 09:45:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,14 +67,13 @@ if (sign_Up) {
           P_WorngSuccess.classList.remove("d-none");
           P_WorngExsist.classList.add("d-none");
           P_WorngRequired.classList.add("d-none");
-        
+             clearInputsSignUp();
         }
     }else{
       box_modal.classList.remove('d-none')
     }
 
 
-     clearInputsSignUp();
   });
 }
 function clearInputsSignUp() {
